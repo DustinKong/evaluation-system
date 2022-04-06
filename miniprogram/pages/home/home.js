8431aeee8e3e529a716f0f6d9a5d2212a2656d23
Melrose <qckong23@163.com>
2022-04-06 22:02:36 +0800

    完善登录

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -7,7 +7,11 @@ Page({
   data: {
 
   },
-
+  go(){
+    wx.navigateTo({
+      url: '/pages/index/index',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -5,13 +5,77 @@ Page({
    * 页面的初始数据
    */
   data: {
+      openId:"",
+      userInfo:{},
+      state:0,
+  },
 
+  getOpenId() {
+    wx.showLoading({
+      title: '',
+    });
+   wx.cloud.callFunction({
+      name: 'quickstartFunctions',
+      config: {
+        env: this.data.envId
+      },
+      data: {
+        type: 'getOpenId'
+      }
+    }).then((resp) => {
+      this.setData({
+        haveGetOpenId: true,
+        openId: resp.result.openid,
+        state:1
+      });
+      wx.setStorageSync("openId", resp.result.openid);
+      wx.showToast({
+        title: '登录成功',
+        icon: 'success',
+        duration: 1000//持续的时间
+      })
+     wx.hideLoading();
+   }).catch((e) => {
+      this.setData({
+        showUploadTip: true
+      });
+     wx.hideLoading();
+    });
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    console.log("userInfo")
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          state:2
+        })
+        wx.setStorageSync("userInfo", res.userInfo);
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.setData({
+      openId:wx.getStorageSync('openId')??"",
+      userInfo:wx.getStorageSync('userInfo')??{}
+    })
+    if(this.data.openId){
+      this.setData({
+        state:1
+      })
+    }
+    if(this.data.userInfo&&this.data.openId!=""){
+      this.setData({
+        state:2
+      })
+    }
 
   },
 
