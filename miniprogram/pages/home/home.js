10ee771dabce8319aea5963c470bd1c185500d85
Melrose <qckong23@163.com>
2022-04-19 21:55:21 +0800

    Update home.js

--- miniprogram/pages/home/home.js
+++ miniprogram/pages/home/home.js
@@ -92,16 +92,16 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    this.setData({
-      type:wx.getStorageSync('type')
-    })
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-      
+    this.setData({
+      type:wx.getStorageSync('type')
+    })
   },
 
   /**
